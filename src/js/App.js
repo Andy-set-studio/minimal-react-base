1ac59b73fe1853556e22721b52cfaba74bd542b0
Andy Bell <me@andy-bell.co.uk>
2020-01-07 13:16:46 +0000

    Add some sensible defaults

--- src/js/App.js
+++ src/js/App.js
@@ -4,6 +4,10 @@ import Example from './components/Example.js';
 
 const App = () => (
   <div className="app">
+    <h1>Hello, I’m your app, using the minmal React app base.</h1>
+    <p>
+      Head over to <a href="//github.com/hankchizljaw/minimal-react-base">the repo</a> to find out more info.
+    </p>
     <Example text="Hi, pal 👋" label="Click me" />
   </div>
 );
