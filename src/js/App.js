a7ae192a885bbfa68d9d112ebbf81473fdfdba26
Andy Bell <me@andy-bell.co.uk>
2020-01-07 13:39:21 +0000

    Use an article and set links to be current color

--- src/js/App.js
+++ src/js/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 import Example from './components/Example.js';
 
 const App = () => (
-  <div className="app">
+  <article>
     <h1>Hello, I’m your app, using the minmal React app base.</h1>
     <p>
       Head over to <a href="//github.com/hankchizljaw/minimal-react-base">the repo</a> to find out more info.
     </p>
     <Example text="Hi, pal 👋" label="Click me" />
-  </div>
+  </article>
 );
 
 export default App;
